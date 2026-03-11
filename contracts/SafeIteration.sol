59ebabc22dcb4cfc413075e50c5d3823eaac7f60
LiamZhuangDev <bzhuangliang@gmail.com>
2026-03-11 15:09:13 -0700

    adds gas-optimized remove functions

--- contracts/SafeIteration.sol
+++ contracts/SafeIteration.sol
@@ -27,6 +27,35 @@ contract SafeIteration {
         return total;
     }
 
+    function removeOrdered(uint index) public {
+        uint len = data.length;
+        require(index < len, "index out of bounds");
+
+        for (uint i = index; i < len - 1;) {
+            data[i] = data[i + 1];
+            unchecked { i++; }
+        }
+
+        data.pop();
+    }
+
+    function deleteRangeOrderPreserved(uint start, uint count) public
+    {
+        uint len = data.length;
+        require(start + count < len, "param out of bounds");
+
+        uint newLen = len - count; // the element with index (len - count - 1) is the last one we need to shift to left
+        for (uint i = start; i < newLen;)
+        {
+            data[i] = data[i + count];
+            unchecked { i++; }
+        }
+
+        assembly {
+            sstore(data.slot, newLen)
+        }
+    }
+
     /// @notice User-triggered iteration, return a paginated slice of the array.
     function getPage(uint pageNumber, uint pageSize)
         public view returns (uint[] memory) 
