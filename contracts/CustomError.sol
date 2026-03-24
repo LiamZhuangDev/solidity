575512e4c9d713f579cc5473c56c291c7cad834d
LiamZhuangDev <bzhuangliang@gmail.com>
2026-03-24 16:53:21 -0700

    extracts custom error in try-catch

--- contracts/CustomError.sol
+++ contracts/CustomError.sol
@@ -35,4 +35,45 @@ contract CustomError {
 
         assert(sumBeforeTransfer == balanceOf[msg.sender] + balanceOf[to]);
     }
+
+    receive() external payable {}
+}
+
+contract CustomErrorCaller {
+    CustomError private ce;
+
+    constructor() {
+        ce = new CustomError();
+    }
+
+    function testTransfer(address to, uint256 amount) public {
+        try ce.transfer(to, amount) {
+            // success case
+        } catch (bytes memory reason) {
+            bytes4 selector;
+
+            // memory layout of reason:
+            // [0x00 - 0x1f] → length (N)
+            // [0x20 - ... ] → actual data (N bytes)
+            if (reason.length >= 4) {
+                assembly {
+                    // skip the first 32 bytes and points to the start of actual data
+                    // mload returns 32 bytes which extracts the error selector: [selector (4 bytes) | rest of data (28 bytes)]
+                    selector := mload(add(reason, 32))
+                }
+            }
+
+            if (selector == CustomError.InsufficientBalance.selector) {
+                // handle InsufficientBalance error
+            } else if (selector == CustomError.InvalidAddress.selector) {
+                // handle InvalidAddress error
+            } else if (selector == CustomError.AmountTooLow.selector) {
+                // handle AmountTooLow error
+            } else if (selector == CustomError.TransferFailed.selector) {
+                // handle TransferFailed error
+            } else {
+                // unknown error
+            }
+        }
+    }
 }
