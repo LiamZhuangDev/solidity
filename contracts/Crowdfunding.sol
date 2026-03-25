13730cbbf1a3c629455be1bdc8b5ea125644435a
LiamZhuangDev <bzhuangliang@gmail.com>
2026-03-25 10:44:30 -0700

    adds custom errors to crowdfunding

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -9,6 +9,13 @@ contract Crowdfunding {
         Withdrawn // funds claimed by owner
     }
 
+    error FundraisingStillActive();
+    error FundraisingEnded();
+    error InvalidAmount(uint256 amount);
+    error Unauthorized(address addr);
+    error RefundFailed(address addr);
+    error WithdrawFailed(address addr);
+    
     State public currState = State.Active;
 
     address public owner;
@@ -24,21 +31,33 @@ contract Crowdfunding {
     }
 
     constructor(uint _goal, uint _duration) {
+        require(_goal > 0, "The goal must be greater than 0");
+        require(_duration > 0, "The duration must be longer than 0");
+
         owner = msg.sender;
         goal = _goal;
         deadline = block.timestamp + _duration;
     }
 
     function contribute() external payable inState(State.Active) {
-        require(block.timestamp < deadline, "Fundraising ended");
-        require(msg.value > 0, "Must send ETH");
+        if (block.timestamp >= deadline) {
+            revert FundraisingEnded();
+        }
+        if (msg.value == 0) {
+            revert InvalidAmount(msg.value);
+        }
+        // require(block.timestamp < deadline, "Fundraising ended");
+        // require(msg.value > 0, "Must send ETH");
 
         fundsContributed[msg.sender] += msg.value;
         totalFunded += msg.value;
     }
 
     function checkGoal() public inState(State.Active) {
-        require(block.timestamp >= deadline, "Fundraising not ended");
+        if (block.timestamp >= deadline) {
+            revert FundraisingEnded();
+        }
+        // require(block.timestamp >= deadline, "Fundraising not ended");
         if (totalFunded >= goal) {
             currState = State.Success;
         } else {
@@ -47,15 +66,28 @@ contract Crowdfunding {
     }
 
     function withdraw() public inState(State.Success) {
-        require(block.timestamp >= deadline, "Fundraising not ended");
+        // require(block.timestamp >= deadline, "Fundraising not ended");
+        if (block.timestamp < deadline) {
+            revert FundraisingStillActive();
+        }
+        if (msg.sender != owner) {
+            revert Unauthorized(msg.sender);
+        }
+        
         (bool success, ) = payable(owner).call{value: address(this).balance}("");
-        require(success, "withdraw failed");
+        // require(success, "withdraw failed");
+        if (!success) {
+            revert WithdrawFailed(owner);
+        }
     }
 
     // Checks-Effects-Interactions pattern
     function refund() public inState(State.Failed) {
         // checks
-        require(block.timestamp >= deadline, "Fundraising not ended");
+        // require(block.timestamp >= deadline, "Fundraising not ended");
+        if (block.timestamp < deadline) {
+            revert FundraisingEnded();
+        }
         uint amount = fundsContributed[msg.sender];
         require(amount > 0, "No funds to refund");
 
@@ -64,7 +96,10 @@ contract Crowdfunding {
 
         // interactions
         (bool success, ) = payable(msg.sender).call{value: amount}("");
-        require(success, "refund failed");
+        // require(success, "refund failed");
+        if (!success) {
+            revert RefundFailed(msg.sender);
+        }
     }
 
     function getProgress() public view returns (uint percentage) {
